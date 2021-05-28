b822d21724c42eba371e62081b88ca37d3937197
Lia Coleman <liailiad@gmail.com>
2021-05-28 15:58:49 -0700

    add base64 img support

--- face_alignment.py
+++ face_alignment.py
@@ -6,42 +6,154 @@ import sys
 
 import numpy as np
 from PIL import Image
+import io
 
-def run_alignment(image_path):
+from argparse import ArgumentParser
+import time
+import numpy as np
+import PIL
+import PIL.Image
+import scipy
+import scipy.ndimage
+import dlib
+import multiprocessing as mp
+import math
+import base64
+
+def get_landmark(img, predictor):
+    """get landmark with dlib
+    :return: np.array shape=(68, 2)
+    """
+    detector = dlib.get_frontal_face_detector()
+
+    dets = detector(img, 1)
+
+    for k, d in enumerate(dets):
+        shape = predictor(img, d)
+
+    t = list(shape.parts())
+    a = []
+    for tt in t:
+        a.append([tt.x, tt.y])
+    lm = np.array(a)
+    return lm
+
+
+def align_face(img, predictor):
+    """
+    :param img: PIL Image
+    :return: PIL Image
+    """
+
+    img_array = np.asarray(img)
+    lm = get_landmark(img_array, predictor)
+
+    lm_chin = lm[0: 17]  # left-right
+    lm_eyebrow_left = lm[17: 22]  # left-right
+    lm_eyebrow_right = lm[22: 27]  # left-right
+    lm_nose = lm[27: 31]  # top-down
+    lm_nostrils = lm[31: 36]  # top-down
+    lm_eye_left = lm[36: 42]  # left-clockwise
+    lm_eye_right = lm[42: 48]  # left-clockwise
+    lm_mouth_outer = lm[48: 60]  # left-clockwise
+    lm_mouth_inner = lm[60: 68]  # left-clockwise
+
+    # Calculate auxiliary vectors.
+    eye_left = np.mean(lm_eye_left, axis=0)
+    eye_right = np.mean(lm_eye_right, axis=0)
+    eye_avg = (eye_left + eye_right) * 0.5
+    eye_to_eye = eye_right - eye_left
+    mouth_left = lm_mouth_outer[0]
+    mouth_right = lm_mouth_outer[6]
+    mouth_avg = (mouth_left + mouth_right) * 0.5
+    eye_to_mouth = mouth_avg - eye_avg
+
+    # Choose oriented crop rectangle.
+    x = eye_to_eye - np.flipud(eye_to_mouth) * [-1, 1]
+    x /= np.hypot(*x)
+    x *= max(np.hypot(*eye_to_eye) * 2.0, np.hypot(*eye_to_mouth) * 1.8)
+    y = np.flipud(x) * [-1, 1]
+    c = eye_avg + eye_to_mouth * 0.1
+    quad = np.stack([c - x - y, c - x + y, c + x + y, c + x - y])
+    qsize = np.hypot(*x) * 2
+
+    output_size = 256
+    transform_size = 256
+    enable_padding = True
+
+    # Shrink.
+    shrink = int(np.floor(qsize / output_size * 0.5))
+    if shrink > 1:
+        rsize = (int(np.rint(float(img.size[0]) / shrink)), int(np.rint(float(img.size[1]) / shrink)))
+        img = img.resize(rsize, PIL.Image.ANTIALIAS)
+        quad /= shrink
+        qsize /= shrink
+
+    # Crop.
+    border = max(int(np.rint(qsize * 0.1)), 3)
+    crop = (int(np.floor(min(quad[:, 0]))), int(np.floor(min(quad[:, 1]))), int(np.ceil(max(quad[:, 0]))),
+            int(np.ceil(max(quad[:, 1]))))
+    crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, img.size[0]),
+            min(crop[3] + border, img.size[1]))
+    if crop[2] - crop[0] < img.size[0] or crop[3] - crop[1] < img.size[1]:
+        img = img.crop(crop)
+        quad -= crop[0:2]
+
+    # Pad.
+    pad = (int(np.floor(min(quad[:, 0]))), int(np.floor(min(quad[:, 1]))), int(np.ceil(max(quad[:, 0]))),
+           int(np.ceil(max(quad[:, 1]))))
+    pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - img.size[0] + border, 0),
+           max(pad[3] - img.size[1] + border, 0))
+    if enable_padding and max(pad) > border - 4:
+        pad = np.maximum(pad, int(np.rint(qsize * 0.3)))
+        img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
+        h, w, _ = img.shape
+        y, x, _ = np.ogrid[:h, :w, :1]
+        mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w - 1 - x) / pad[2]),
+                          1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h - 1 - y) / pad[3]))
+        blur = qsize * 0.02
+        img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
+        img += (np.median(img, axis=(0, 1)) - img) * np.clip(mask, 0.0, 1.0)
+        img = PIL.Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
+        quad += pad[:2]
+
+    # Transform.
+    img = img.transform((transform_size, transform_size), PIL.Image.QUAD, (quad + 0.5).flatten(), PIL.Image.BILINEAR)
+    if output_size < transform_size:
+        img = img.resize((output_size, output_size), PIL.Image.ANTIALIAS)
+
+    # Save aligned image.
+    return img
+
+
+def run_alignment(base64imgstring):
     import dlib
-    from scripts.align_faces_parallel import align_face
+    # from scripts.align_faces_parallel import align_face
+
+    # parse base64 img string to img array
+    binary_img = base64.b64decode(base64imgstring)
+    img_PIL = Image.open(io.BytesIO(binary_img))
+    img_PIL 
+    
+    time1 = time.time()
     if not os.path.exists("shape_predictor_68_face_landmarks.dat"):
         print('Downloading files for aligning face image...')
         os.system('wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2')
         os.system('bzip2 -dk shape_predictor_68_face_landmarks.dat.bz2')
         print('Done.')
+
+    time2 = time.time()
+    print('Time to get shape_predictor file took {:.4f} seconds.'.format(time2 - time1))
+
     predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-    aligned_image = align_face(filepath=image_path, predictor=predictor) 
+    aligned_image = align_face(img=img_PIL, predictor=predictor) 
     print("Aligned image has shape: {}".format(aligned_image.size))
-    return aligned_image 
+    time3 = time.time()
+    print('Time to align image took {:.4f} seconds.'.format(time3 - time2))
 
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='Align a square RGB face image',
-        formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-
-    parser.add_argument('--image_path',  help='Input path to face image, needs to be square and RGB', dest='image_path', required=True)
-    parser.add_argument('--output_path',  help='Output file path, will be saved as a 256px x 256px image. JPG will work.', dest='output_path', required=True)
-  
-    # make sure output folder exists, otherwise saving won’t work
-    if not os.path.exists('./face_alignment_outputs/'):
-        os.makedirs('./face_alignment_outputs/')
-
-    args = parser.parse_args()
-    
-    aligned_image = run_alignment(args.image_path)
-
-    aligned_image.save(args.output_path)
-
-#----------------------------------------------------------------------------
-
-if __name__ == "__main__":
-    main()
+    # parse PIL image back out to base64 string
+    buffered = io.BytesIO()
+    aligned_image.save(buffered, format="PNG")
+    aligned_image_base64str = base64.b64encode(buffered.getvalue())
+    return aligned_image_base64str 
 
